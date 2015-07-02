c9b9fc268f2cf74b2cf505e56c4476c664cb3ada
Nelson Conover <njc46@cornell.edu>
2015-07-01 23:07:21 -0400

    more work

--- index.jl
+++ index.jl
@@ -9,13 +9,12 @@ function main(window)
 
     # mytitle = Escher.title(4, "OrangeQuarry")
     mytitle = title(4, "OrangeQuarry")
-    rec = rectangle()
-
-    mytitle = vbox(hskip(2em), mytitle)
     mytitle = vbox(vskip(2em), mytitle)
 
+    rec = Compose.rectangle([0], [0], [1], [1])
+    logo = compose(context(0, 0), rec, fill("tomato"))
+    # logo = compose(context(), rec, fill("tomato"))
 
-    logo = compose(context(0, 0, .75, 1), rec, fill("tomato"))
     logo = logo |> padThing
 
 
@@ -34,10 +33,18 @@ function main(window)
             """ 
     
 
-    triangle = compose(context(), polygon([(1,1), (0,1), (1/2, 0)]))
+
+    triangle = compose(
+            context(),
+            circle([0.25, 0.5, 0.75], [0.25, 0.5, 0.75], [0.1, 0.1, 0.1]),
+            fill(LCHab(92, 10, 97))
+            )
+
+    introspect(triangle)
+
     todolist = hbox([hskip(4em), todolist, hskip(10em), triangle])
 
-    all_the_things = vbox([header, todolist, vskip(10em)])
+    all_the_things = vbox([header, todolist, vskip(8em)])
     all_the_things = fillcolor("#FDFBF9", all_the_things)
     all_the_things
 
